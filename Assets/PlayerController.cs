f1eac08106eb31a7924e3cecbfb090cdeb2e1f34
acumella <80059543+acumella@users.noreply.github.com>
2021-03-06 04:45:31 +0100

    Added mouse collision detection

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -17,8 +17,9 @@ public class PlayerController : MonoBehaviour
     private float timeJumpCounter;
 
     private float moveSide;
-    private bool jumped = false;
     private bool jumpReleased = true;
+
+    bool leftPressed = false;
 
     private void Awake()
     {
@@ -38,6 +39,21 @@ public class PlayerController : MonoBehaviour
         anim = GetComponent<Animator>();
     }
 
+    void Update()
+    {
+        CheckGround();
+        MouseHit();
+    }
+
+
+    void FixedUpdate()
+    {
+        Jump();
+        Move();
+        Turn();      
+    }
+
+    //====================== PLAYER INPUT ========================
     void OnMove(InputValue input)
     {
         moveSide = input.Get<Vector2>().x;
@@ -45,51 +61,85 @@ public class PlayerController : MonoBehaviour
 
     void OnJumpPress()
     {
-        if (isGrounded) { 
+        if (isGrounded)
+        {
             jumpReleased = false;
-            jumped = true;
         }
     }
 
     void OnJumpRelease()
     {
         jumpReleased = true;
-        jumped = false;        
     }
 
-    void Update()
+    void OnLeftPressed()
     {
-        if(isGrounded)
+        leftPressed = true;
+    }
+
+    void OnLeftReleased()
+    {
+        leftPressed = false;
+    }
+
+    //====================== FUNCTIONS ==============================
+
+    private void CheckGround()
+    {
+        if (isGrounded)
         {
             timeJumpCounter = timeJump;
             anim.SetBool("jumping", false);
         }
     }
 
+    private void MouseHit()
+    {
+        if (leftPressed)
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
 
-    void FixedUpdate()
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+
+            if (hit.collider != null)
+            {
+                Debug.Log(hit.collider.gameObject.name);
+            }
+        }
+    }
+
+    private void Jump()
     {
         if (timeJumpCounter > 0 && !jumpReleased)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
             anim.SetBool("jumping", true);
             timeJumpCounter -= Time.deltaTime;
-            jumped = false;
         }
+    }
 
+    private void Move()
+    {
         rb.velocity = new Vector2(moveSide * speed, rb.velocity.y);
+    }
 
+    private void Turn()
+    {
         if (rb.velocity[0] < 0)
         {
             transform.localScale = new Vector2(-1, 1);
             anim.SetBool("running", true);
-        } else if(rb.velocity[0] > 0)
+        }
+        else if (rb.velocity[0] > 0)
         {
             transform.localScale = new Vector2(1, 1);
             anim.SetBool("running", true);
-        } else {
+        }
+        else
+        {
             anim.SetBool("running", false);
         }
     }
-    
+
 }
