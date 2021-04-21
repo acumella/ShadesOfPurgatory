56fffc0da94625a6e05a891e12494f46dcb16f1e
acumella <80059543+acumella@users.noreply.github.com>
2021-04-21 20:50:55 +0200

    Added Pause Menu

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool isPaused = false;
+    public GameObject pauseMenuUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (isPaused) Resume();
+            else Pause();
+        }    
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    private void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0;
+        isPaused = true;
+    }
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+        Resume();
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
